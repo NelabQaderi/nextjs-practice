a51d8660edabd6f7b8eb05a324bddc02dd95f959
NelabQaderi <nelabqaderi1122@gmail.com>
2024-05-14 23:51:47 +0430

    please cheack it

--- components/Molecules/Form/Form.jsx
+++ components/Molecules/Form/Form.jsx
@@ -1,15 +1,38 @@
 'use client'
 
-import { Box, Input } from "@mantine/core";
+import { Box, Button, Input, PasswordInput } from "@mantine/core";
+import { IconSend } from "@tabler/icons-react";
+import { useState } from "react";
 
 export default function Form() {
+
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+   
+
+    const handleClick = () => {
+        console.log(username, email, password)
+    }
+
     return (
         <Box>
-            <Form>
+            <form>
                 <Input.Wrapper label="user name">
-                     <Input placeholder="entar your name" />
+                     <Input onChenge={(e) => setUsername(e.target.value)} placeholder="entar your name" />
                 </Input.Wrapper>
-            </Form>
+
+                <Input.Wrapper label="user Email">
+                     <Input onChenge={(e) => setEmail(e.target.value)} placeholder="entar your Email" />
+                </Input.Wrapper>
+
+                <Input.Wrapper label="password">
+                     <PasswordInput onChenge={(e) => setPassword(e.target.value)} placeholder="entar your passord" />
+                </Input.Wrapper>
+
+                <Button onClick={() => handleClick()} fullWidth leftSection ={s<IconSend />} mt={40}>click me</Button>
+
+            </form>
         </Box>
     )
 }
