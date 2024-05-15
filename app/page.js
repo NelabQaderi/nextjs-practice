247f0d0d2133297d986361976ebd9f6035d576f8
NelabQaderi <nelabqaderi1122@gmail.com>
2024-05-15 13:05:13 +0430

    please cheack it

--- app/page.js
+++ app/page.js
@@ -1,10 +1,20 @@
+import DarkMode from "@/components/Atoms/DarkMode/DarkMode";
 import  Form from "@/components/Molecules/Form/Form";
+import { Box } from "@mantine/core";
 
 export default function Home() {
 
    return (
      <div>
-        <Form/>
+         <Box style={{display: 'flex', flexDirection: 'row-reverse'}}>
+            <DarkMode/> 
+         </Box>
+
+         <Box mt={20}>
+            <Form/>
+         </Box>
+
      </div>
   );
 }
+ 
--- components/Molecules/Form/Form.jsx
+++ components/Molecules/Form/Form.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Box, Button, Input, PasswordInput } from "@mantine/core";
+import { Box, Button, Input, PasswordInput, Select, Table } from "@mantine/core";
 import { IconSend } from "@tabler/icons-react";
 import { useState } from "react";
 
@@ -9,30 +9,78 @@ export default function Form() {
     const [username, setUsername] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [gender, setGender] = useState("")
+    const [allData, setAllData] = useState({
+        username: "",
+        email: "",
+        password: "",
+        gender: ""
+    })
+        
    
 
     const handleClick = () => {
-        console.log(username, email, password)
-    }
+       setAllData({
+        username: username,
+        email: email,
+        password: password,
+        gender: gender
+        })
+    }    
 
     return (
         <Box>
             <form>
                 <Input.Wrapper label="user name">
-                     <Input onChenge={(e) => setUsername(e.target.value)} placeholder="entar your name" />
+                     <Input onChange={(e) => setUsername(e.target.value)} placeholder="entar your name" />
                 </Input.Wrapper>
 
                 <Input.Wrapper label="user Email">
-                     <Input onChenge={(e) => setEmail(e.target.value)} placeholder="entar your Email" />
+                     <Input onChange={(e) => setEmail(e.target.value)} placeholder="entar your Email" />
                 </Input.Wrapper>
 
                 <Input.Wrapper label="password">
-                     <PasswordInput onChenge={(e) => setPassword(e.target.value)} placeholder="entar your passord" />
+                     <PasswordInput onChange={(e) => setPassword(e.target.value)} placeholder="entar your passord" />
                 </Input.Wrapper>
 
-                <Button onClick={() => handleClick()} fullWidth leftSection ={s<IconSend />} mt={40}>click me</Button>
+                <Input.Wrapper label="gender">
+                    <Select onChange={(e) => setGender(e)} data={['Male', 'Female']} />
+                </Input.Wrapper>
 
+                <Button onClick={() => handleClick()} fullWidth leftSection ={ <IconSend />} mt={40}>Send</Button>
+                 
+               
             </form>
+
+            <Table mt={60}>
+                <Table.Thead>
+                    <Table.Tr>
+                        <Table.Th>Username</Table.Th>
+                        <Table.Th>Email</Table.Th>
+                        <Table.Th>password</Table.Th>
+                        <Table.Th>gender</Table.Th>
+                    </Table.Tr>
+               </Table.Thead>
+
+               <Table.Tbody>
+                   <Table.Tr>
+                       <Table.Td>
+                           {allData.username}
+                       </Table.Td>
+                       <Table.Td>
+                           {allData.email}
+                       </Table.Td>
+                       <Table.Td>
+                           {allData.password}
+                       </Table.Td>
+                       <Table.Td>
+                           {allData.gender}
+                       </Table.Td>
+                   </Table.Tr>
+               </Table.Tbody>
+
+            </Table>
+      
         </Box>
     )
 }
